b22a9248382fc5d07c71e93b4067c8cc289de4d3
Sumanth NR <sumanthnr62@gmail.com>
2023-10-13 01:29:27 +0530

    Minor Changes

--- Debug/brute.cpp
+++ Debug/brute.cpp
@@ -1,42 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 void solve() {
-    int n, nQ;
-    cin >> n >> nQ;
 
-    string str;
-    cin >> str;
-
-    for (int t, l, r, x; nQ--; ) {
-        cin >> t >> l >> r;
-        if (t == 1) {
-            cin >> x;
-            x %= 26;
-            for (int i = l - 1; i <= r - 1; ++i) {
-                str[i] = (str[i] - 'a' + x) % 26 + 'a';
-            }
-        } else if (t == 2) {
-            bool ok = true;
-            for (int i = l - 1; i <= r - 1; ++i) {
-                if (i + 1 < r) {
-                    if (str[i] == str[i + 1]) {
-                        ok = false;
-                        break;
-                    }
-                }
-                if (i + 2 < r) {
-                    if (str[i] == str[i + 2]) {
-                        ok = false;
-                        break;
-                    }
-                }
-            }
-            cout << (ok ? "YES" : "NO") << '\n';
-        } else {
-            assert(false);
-        }
-    }
 }
 
 int main() {
